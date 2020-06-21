85ad1a6c94afa8f9ec395cf82f3d42c6aa95ae3a
David Delgado <dadch13.14@gmail.com>
2020-06-21 15:02:20 -0400

    Preparación del servidor e instalación de dependencias.

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import boom from '@hapi/boom';
+import passport from 'passport';
+import axios from 'axios';
 
 import React from 'react';
 import { renderToString } from 'react-dom/server';
@@ -21,6 +25,13 @@ dotenv.config();
 const { ENV, PORT } = process.env;
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./utils/auth/strategies/basic');
 
 if (ENV === 'development') {
   console.log('Development config');
@@ -90,6 +101,53 @@ const renderApp = (req, res) => {
 
   res.send(setResponse(html, preloadedState, req.hashManifest));
 };
+
+app.post('/auth/sign-in', async function (req, res, next) {
+  // Obtenemos el atributo rememberMe desde el cuerpo del request
+  const { rememberMe } = req.body;
+
+  passport.authenticate('basic', function (error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error);
+        }
+
+        const { token, ...user } = data;
+
+        res.cookie('token', token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC,
+        });
+
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
+    }
+  })(req, res, next);
+});
+
+app.post('/auth/sign-up', async function (req, res, next) {
+  const { body: user } = req;
+
+  try {
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: 'post',
+      data: user,
+    });
+
+    res.status(201).json({ message: 'User created.' });
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.get('*', renderApp);
 
