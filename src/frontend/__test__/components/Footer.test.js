cd4add94291c0e3672ff89a0fe762b0dcf7f5e67
David Delgado <dadch13.14@gmail.com>
2020-06-21 20:53:45 -0400

    Implementando snapshots

--- src/frontend/__test__/components/Footer.test.js
+++ src/frontend/__test__/components/Footer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
@@ -11,5 +12,10 @@ describe('<Footer />', () => {
 
   test('Footer haves three anchors', () => {
     expect(footer.find('a')).toHaveLength(3);
+  });
+
+  test('Footer Snapshot', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
