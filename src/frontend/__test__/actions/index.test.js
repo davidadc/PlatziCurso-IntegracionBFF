25c2d6fdc9556df8b807d1fd5b2e0140b858e4c8
David Delgado <dadch13.14@gmail.com>
2020-06-28 13:57:32 -0400

    Probando acciones

---
+++ src/frontend/__test__/actions/index.test.js
@@ -0,0 +1,28 @@
+import { loginRequest, setFavorite } from '../../actions';
+import movieMock from '../../__mocks__/movieMock';
+
+describe('Actions', () => {
+  test('Set Favorite', () => {
+    const payload = movieMock;
+    const expectedAction = {
+      type: 'SET_FAVORITE',
+      payload,
+    };
+
+    expect(setFavorite(payload)).toEqual(expectedAction);
+  });
+
+  test('Login', () => {
+    const payload = {
+      email: 'test@test.com',
+      password: 'password',
+    };
+
+    const expectedAction = {
+      type: 'LOGIN_REQUEST',
+      payload,
+    };
+
+    expect(loginRequest(payload)).toEqual(expectedAction);
+  });
+});
